3a5f8e5d45f552ab4ff9d53730e04bcad620b912
Jiayoushi <jyshi@uci.edu>
2019-12-19 21:21:20 -0500

    cmake

--- src/server/srt_server_overlay.h
+++ src/server/srt_server_overlay.h
@@ -1,6 +1,20 @@
 #ifndef AIR_SRT_SERVER_OVERLAY_H_
 #define AIR_SRT_SERVER_OVERLAY_H_
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
 
 /*       
  *      my protocol
---
+++ tests/unit_test.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "server/srt_server.h"
+#include "client/srt_client.h"
+#include "server/srt_server_overlay.h"
+#include "client/srt_client_overlay.h"
+
+void test() {
+
+
+}
+
+int main() {
+  test();
+
+  return 0;
+}
