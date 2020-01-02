886f06aeeef1a58eca0d3fdd60dad0182d3299fb
Shi Jiayou <js3896@zoo.cs.yale.edu>
2020-01-01 19:19:10 -0500

    connection test

--- tests/con_client.cc
+++ tests/con_client.cc
@@ -5,61 +5,59 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <netdb.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-#include "common/constants.h"
-#include "srt_client.h"
+#include "client/srt_client.h"
+#include "air/air.h"
 
 #define kClientPort1  8000
 #define kServerPort1  8001
 #define kClientPort2  8002
 #define kServerPort2  8003
 
-const char *hostname = "127.0.0.1";
-unsigned int hostport = 8080;
+const char *hostname = "turtle.zoo.cs.yale.edu";
 
 // After the connections are created, wait for 5 seconds and then close it
 #define kWaitTime      1
 
 
-
 int main() {
-  srand(time(nullptr));
+  Init();
 
-  int overlay_conn = OverlayClientStart(hostname, hostport);
-  if (overlay_conn < 0) {
-    std::cerr << "Failed to start overlay" << std::endl;
-    exit(-1);
-  }
-
-  SrtClientInit(overlay_conn);
+  SrtClientInit();
   std::cerr << "Client init success" << std::endl;
 
   // First connection
-  int sockfd = SrtClientSock(kClientPort1);
+  int sockfd = SrtClientSock();
   if (sockfd < 0) {
     std::cerr << "Failed to create srt client sock" << std::endl;
     exit(-1);
   }
   std::cerr << "socket created: " << sockfd << std::endl;
 
-  if (SrtClientConnect(sockfd, kServerPort1) < 0) {
+  struct hostent *he = gethostbyname(hostname);
+  struct sockaddr_in server_addr;
+  memcpy(&server_addr.sin_addr, he->h_addr_list[0], he->h_length);
+  uint32_t server_ip = server_addr.sin_addr.s_addr;
+
+  if (SrtClientConnect(sockfd, server_ip, kServerPort1) < 0) {
     std::cerr << "Failed to connect to srt server" << std::endl;
     exit(-1);
   }
   printf("Client port [%d] connects to server [%d]\n", kClientPort1, kServerPort1);
 
   // Second connection
-  int sockfd2 = SrtClientSock(kClientPort2);
+  int sockfd2 = SrtClientSock();
   if (sockfd2 < 0) {
     std::cerr << "Failed to create srt client sock" << std::endl;
     exit(-1);
   }
 
-  if (SrtClientConnect(sockfd2, kServerPort2) < 0) {
+  if (SrtClientConnect(sockfd2, server_ip, kServerPort2) < 0) {
     std::cerr << "Failed to connect to srt server" << std::endl;
     exit(-1);
   }
@@ -70,14 +68,14 @@ int main() {
 
   std::cerr << "Attempt to close sockfd #1 " << std::endl;
   // Close first connection
-  if (SrtClientDisconnect(sockfd)<0) {
+  if (SrtClientDisconnect(sockfd) < 0) {
     std::cerr << "Failed to disconnect from srt server" << std::endl;
     exit(EXIT_FAILURE);
   }
   std::cerr << "connection #1 closed " << std::endl;
 
   // Close second connection
-  if (SrtClientClose(sockfd)<0) {
+  if (SrtClientClose(sockfd) < 0) {
     std::cerr << "Failed to close srt client" << std::endl;
     exit(EXIT_FAILURE);
   }
@@ -99,8 +97,8 @@ int main() {
   SrtClientShutdown();
   std::cerr << "shutdown " << std::endl;
 
-  OverlayClientStop(overlay_conn);
-  std::cerr << "overlay stop" << std::endl;
+  Stop();
+  std::cout << "Stopped" << std::endl;
 
   return 0;
 }
--- tests/con_server.cc
+++ tests/con_server.cc
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -11,49 +12,64 @@
 #include <time.h>
 #include <iostream>
 
-#include "common/constants.h"
-#include "srt_server.h"
+#include "server/srt_server.h"
+#include "air/air.h"
 
 #define kClientPort1 8000
 #define kServerPort1 8001
 #define kClientPort2 8002
 #define kServerPort2 8003
 
-#define kWaitTime    100
+#define kWaitTime    10
 
-const char *hostname = "127.0.0.1";
-unsigned int hostport = 8080;
+const char *hostname = "turtle.zoo.cs.yale.edu";
 
 int main() {
-  srand(time(nullptr));
+  Init();
 
-  int overlay_conn = OverlayServerStart(hostname, hostport);
-  if (overlay_conn < 0) {
-    std::cerr << "can not start overlay" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  SrtServerInit(overlay_conn);
+  SrtServerInit();
   std::cerr << "Server Init success" << std::endl;
 
   // Open 1
-  int sockfd = SrtServerSock(kServerPort1);
+  int sockfd = SrtServerSock();
   if (sockfd < 0) {
     std::cerr << "can't create srt socket1" << std::endl;
     exit(EXIT_FAILURE);
   }
   std::cerr << "socket created: " << sockfd << std::endl;
 
-  SrtServerAccept(sockfd);
+  struct hostent *he = gethostbyname(hostname);
+  struct sockaddr_in server_addr;
+  memcpy(&server_addr.sin_addr, he->h_addr_list[0], he->h_length);
+  uint32_t server_ip = server_addr.sin_addr.s_addr;
+
+  if (SrtServerBind(sockfd, server_ip, kServerPort1) < 0) {
+    std::cerr << "srt_server_bind #1 failed" << std::endl;
+    exit(-1);
+  }
+
+  if (SrtServerAccept(sockfd) < 0) {
+    std::cerr << "srt_server accept #1 failed" << std::endl;
+    exit(-1);
+  }
   std::cerr << "Accept #1 succeed" << std::endl;
 
   // Open 2
-  int sockfd2 = SrtServerSock(kServerPort2);
+  int sockfd2 = SrtServerSock();
   if (sockfd2 < 0) {
     std::cerr << "can't create srt socket2" << std::endl;
     exit(EXIT_FAILURE);
   }
-  SrtServerAccept(sockfd2);
+
+  if (SrtServerBind(sockfd2, server_ip, kServerPort2) < 0) {
+    std::cerr << "srt_server_bind #2 failed" << std::endl;
+    exit(-1);
+  }
+
+  if (SrtServerAccept(sockfd2) < 0) {
+    std::cerr << "srt_server accept #2 failed" << std::endl;
+    exit(-1);
+  }
   std::cerr << "Accept #2 succeed" << std::endl;
 
   // Sleep
@@ -77,8 +93,8 @@ int main() {
   SrtServerShutdown();
   std::cerr << "shutdown " << std::endl;
 
-  OverlayServerStop(overlay_conn);
-  std::cerr << "overlay stop" << std::endl;
+  Stop();
+  std::cout << "Stopped" << std::endl;
 
   return 0;
 }
